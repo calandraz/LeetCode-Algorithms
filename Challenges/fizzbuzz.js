915e1c724c534ff79c5950b8e93b59b3af8762ca
Calandra Zellner <calandrazellner@gmail.com>
2017-01-02 15:07:54 -0500

    update
    update

--- Challenges/fizzbuzz.js
+++ Challenges/fizzbuzz.js
@@ -1,4 +1,4 @@
-/* LeetCode Challenge FIZZ Buzz
+/* LeetCode Challenge FIZZ Buzz---Difficulty Easy
     Instructions: 
     Write a program that outputs the string representation of numbers from 1 to n.
     But for multiples of three it should output “Fizz” instead of the number and for the multiples of five output “Buzz”. 
